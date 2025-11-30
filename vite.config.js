0e8000be5a3230411eaae62cbfd090ecc5b09a97
werttart <30403788+werttart@users.noreply.github.com>
2025-11-30 11:01:51 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // สั่งให้สร้างโฟลเดอร์ชื่อ 'build' แทน 'dist' (เพื่อให้ Vercel เจอ)
+  }
 })
